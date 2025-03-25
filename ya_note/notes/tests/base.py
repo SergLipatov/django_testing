808c942379783415892ea6349e8f340402ac5617
Serg Lipatov <478997@mail.ru>
2025-03-25 14:52:00 +0400

    After_review_and_add_corr

--- ya_note/notes/tests/base.py
+++ ya_note/notes/tests/base.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-
 from notes.models import Note
 from .urls import detail_url, edit_url, delete_url
 
@@ -10,6 +9,17 @@ User = get_user_model()
 class BaseTestCase(TestCase):
     """Базовый тестовый класс с фикстурами и вспомогательными методами."""
 
+    NEW_NOTE_DATA = {
+        "title": "Новая заметка",
+        "text": "Какой-то текст",
+        "slug": "new-note"
+    }
+
+    EDIT_NOTE_DATA = {
+        "title": "Обновлённая заметка",
+        "text": "Обновлённый текст",
+    }
+
     @classmethod
     def setUpTestData(cls):
         """Создаёт тестовых пользователей и их заметки."""
@@ -18,13 +28,14 @@ class BaseTestCase(TestCase):
 
         cls.author_note = Note.objects.create(
             title="Заметка автора",
-            text="Текст заметки",
+            text="Текст 1",
             author=cls.author,
             slug="author-note"
         )
+
         cls.other_user_note = Note.objects.create(
             title="Заметка другого пользователя",
-            text="Текст заметки",
+            text="Текст 2",
             author=cls.other_user,
             slug="other-note"
         )
@@ -32,7 +43,6 @@ class BaseTestCase(TestCase):
         cls.AUTHOR_DETAIL_URL = detail_url(cls.author_note.slug)
         cls.AUTHOR_EDIT_URL = edit_url(cls.author_note.slug)
         cls.AUTHOR_DELETE_URL = delete_url(cls.author_note.slug)
-
         cls.OTHER_DETAIL_URL = detail_url(cls.other_user_note.slug)
         cls.OTHER_EDIT_URL = edit_url(cls.other_user_note.slug)
         cls.OTHER_DELETE_URL = delete_url(cls.other_user_note.slug)
--- ya_note/notes/tests/test_logic.py
+++ ya_note/notes/tests/test_logic.py
@@ -1,3 +1,4 @@
+from http import HTTPStatus
 from notes.models import Note
 from .base import BaseTestCase
 from .urls import ADD_URL, SUCCESS_URL
@@ -12,30 +13,43 @@ class NoteCRUDTests(BaseTestCase):
 
     def test_create_note(self):
         """Авторизованный пользователь может создать заметку."""
-        data = {"title": "Новая заметка", "text": "Текст", "slug": "new-note"}
-        response = self.client.post(ADD_URL, data)
+        response = self.client.post(ADD_URL, self.NEW_NOTE_DATA)
         self.assertRedirects(response, SUCCESS_URL)
-        self.assertTrue(Note.objects.filter(slug="new-note").exists())
-        new_note = Note.objects.get(slug="new-note")
-        self.assertEqual(new_note.title, data["title"])
-        self.assertEqual(new_note.text, data["text"])
+        self.assertEqual(Note.objects.count(), 3)  # + две стартовые
+        new_note = Note.objects.get(slug=self.NEW_NOTE_DATA["slug"])
+        self.assertEqual(new_note.title, self.NEW_NOTE_DATA["title"])
+        self.assertEqual(new_note.text, self.NEW_NOTE_DATA["text"])
         self.assertEqual(new_note.author, self.author)
 
     def test_edit_note(self):
         """Автор может редактировать свою заметку."""
-        data = {
-            "title": "Обновлённая",
-            "text": "Обновлённый текст",
-            "slug": self.author_note.slug
-        }
-        response = self.client.post(self.AUTHOR_EDIT_URL, data)
+        self.EDIT_NOTE_DATA["slug"] = self.author_note.slug
+        response = self.client.post(self.AUTHOR_EDIT_URL, self.EDIT_NOTE_DATA)
         self.assertRedirects(response, SUCCESS_URL)
-        self.author_note.refresh_from_db()
-        self.assertEqual(self.author_note.title, data["title"])
-        self.assertEqual(self.author_note.text, data["text"])
+        edited_note = Note.objects.get(slug=self.author_note.slug)
+        self.assertEqual(edited_note.title, self.EDIT_NOTE_DATA["title"])
+        self.assertEqual(edited_note.text, self.EDIT_NOTE_DATA["text"])
 
     def test_delete_note(self):
         """Автор может удалить свою заметку."""
         response = self.client.post(self.AUTHOR_DELETE_URL)
         self.assertRedirects(response, SUCCESS_URL)
-        self.assertFalse(Note.objects.filter(id=self.author_note.id).exists())
+        self.assertEqual(Note.objects.count(), 1)  # Чужая остаётся
+
+    def test_cannot_edit_other_user_note(self):
+        """Пользователь НЕ может редактировать чужую заметку."""
+        self.EDIT_NOTE_DATA["slug"] = self.other_user_note.slug
+        response = self.client.post(self.OTHER_EDIT_URL, self.EDIT_NOTE_DATA)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        other_user_note = Note.objects.get(slug=self.other_user_note.slug)
+        self.assertNotEqual(
+            other_user_note.title,
+            self.EDIT_NOTE_DATA["title"]
+        )
+        self.assertNotEqual(other_user_note.text, self.EDIT_NOTE_DATA["text"])
+
+    def test_cannot_delete_other_user_note(self):
+        """Пользователь НЕ может удалить чужую заметку."""
+        response = self.client.post(self.OTHER_DELETE_URL)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        self.assertEqual(Note.objects.count(), 2)
